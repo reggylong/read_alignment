d7381edfa965abef1de89f65630537233388a909
Reginald Long <reggylong@gmail.com>
2016-08-29 23:00:36 -0700

    aligner works for ce.fa...however hg is still too slow

--- src/main.cc
+++ src/main.cc
@@ -19,16 +19,18 @@
 
 #define DEBUG false
 #define VERBOSE false
+#define STATS false
 
 using namespace std;
 
 static const size_t LENGTH = 14;
-static const size_t LOCATIONS = 5;
+static const size_t LOCATIONS = 10;
+static const size_t MIN_COST = 5;
 static const size_t SPACE = pow(4, LENGTH);
 static const long THREADS = 6;
 string complete_ref;
 
-static uint_fast32_t edit_dist_helper(string str1, string str2) {
+static uint_fast32_t edit_dist(string str1, string str2) {
   static vector<vector<uint_fast32_t>> matrix_edits(str1.length() + 1, vector<uint_fast32_t>(str2.length() + 1));
 
   for (uint32_t i = 0; i <= str1.length(); i++) {
@@ -115,6 +117,7 @@ void process_reference(vector<vector<uint_fast32_t>> &m) {
 uint_fast32_t hamming(const string &read, uint_fast32_t pos) {
   uint_fast32_t dist = 0;
   for (size_t i = 0; i < read.length(); i++) {
+    if (pos + i >= complete_ref.length()) return dist + (read.length() - i);
     if (read[i] != complete_ref[pos + i]) dist++;
   }
   return dist;
@@ -126,6 +129,7 @@ void compute_locations(const string &read, vector<vector<uint_fast32_t>> &m, vec
   while (i < read.size() && (i < LENGTH || locations.size() < LOCATIONS)) {
     if (ktoi(read, i, index)) {
       for (uint_fast32_t j = 0; j < m[index].size(); j++) {
+        if (m[index][j] - i + read.length() >= complete_ref.size()) continue; 
         locations.push_back(m[index][j] - i);
       }
     }
@@ -133,8 +137,8 @@ void compute_locations(const string &read, vector<vector<uint_fast32_t>> &m, vec
   }
 }
 
-uint_fast32_t argmin(vector<uint_fast32_t> &v) {
-  uint_fast32_t i = 0;
+int_fast32_t argmin(const vector<uint_fast32_t> &v) {
+  int_fast32_t i = 0;
   uint_fast32_t minimum = UINT_FAST32_MAX;
   for (uint_fast32_t j = 0; j < v.size(); j++) {
     if (v[j] < minimum) {
@@ -145,19 +149,26 @@ uint_fast32_t argmin(vector<uint_fast32_t> &v) {
   return i;
 }
 
-uint_fast32_t compute_hamming(string &read, vector<uint_fast32_t> &locations, vector<uint_fast32_t> &dists) {
-  if (locations.empty()) return UINT_FAST32_MAX;
+int_fast32_t compute_hamming(string &read, vector<uint_fast32_t> &locations, vector<uint_fast32_t> &dists) {
+  if (locations.empty()) {
+    return -1;
+  }
   for (uint_fast32_t i = 0; i < locations.size(); i++) {
     dists.emplace_back(hamming(read, locations[i]));
   }
-  uint_fast32_t index = argmin(dists);
-  return dists[index];
+  int_fast32_t index = argmin(dists);
+  if (VERBOSE) cout << read << endl;
+  if (VERBOSE) cout << complete_ref.substr(locations[index], 100) << endl;
+  return index;
 }
 
 void map_reads(char *fastqname, vector<vector<uint_fast32_t>> &m) {
 
-  vector<uint_fast32_t> locations;
-  vector<uint_fast32_t> reversed_locations;
+  static size_t max_size = 0;
+  static vector<uint_fast32_t> locations;
+  static vector<uint_fast32_t> reversed_locations;
+  locations.reserve(10000);
+  reversed_locations.reserve(10000);
 
   ifstream f;
   f.open(fastqname);
@@ -165,10 +176,12 @@ void map_reads(char *fastqname, vector<vector<uint_fast32_t>> &m) {
   long counter = 0;
   double average = 0.0;
   double dist = 0.0;
+  double edit = 0.0;
+
   long unmapped = 0;
   string name = "";
   while (getline(f, line)) {
-    if (counter % 100000 == 0) {
+    if (counter % 1000 == 0) {
       cout << "Examined " << counter << " lines" << endl;
     }
     if (counter % 4 != 1) {
@@ -180,6 +193,9 @@ void map_reads(char *fastqname, vector<vector<uint_fast32_t>> &m) {
     if (VERBOSE) {
       cout << "[map_reads] Examining line: " << line << endl;
     }
+    vector<uint_fast32_t> dists;
+    vector<uint_fast32_t> reversed_dists;
+
     compute_locations(line, m, locations);
     string reversed_read;
     reverse_read(line, reversed_read);
@@ -198,22 +214,41 @@ void map_reads(char *fastqname, vector<vector<uint_fast32_t>> &m) {
     if (locations.empty() && reversed_locations.empty()) {
       unmapped++;
     } else {
-      vector<uint_fast32_t> dists;
-      vector<uint_fast32_t> reversed_dists;
-      uint_fast32_t distance = min(compute_hamming(line, locations, dists), compute_hamming(reversed_read, reversed_locations, reversed_dists));
+      int_fast32_t locations_index = compute_hamming(line, locations, dists);
+      uint_fast32_t value = (locations_index >= 0) ? dists[locations_index] : UINT_FAST32_MAX;
+      int_fast32_t reversed_index = compute_hamming(reversed_read, reversed_locations, reversed_dists);
+      uint_fast32_t reversed_value = (reversed_index >= 0) ? reversed_dists[reversed_index] : UINT_FAST32_MAX; 
 
-      cout << "[map_reads] " << name << " " << distance << endl;
-      dist += distance;
+      uint_fast32_t h_dist = 0;
+      uint_fast32_t e_dist = 0;
+      // compute edit distance
+      if (value < reversed_value) {
+        if (STATS) e_dist = edit_dist(line, complete_ref.substr(locations[locations_index], line.length()));
+        h_dist = value;
+      } else {
+        if (STATS) e_dist = edit_dist(reversed_read, complete_ref.substr(reversed_locations[reversed_index], reversed_read.length()));
+        h_dist = reversed_value;
+      }
+
+      if (VERBOSE) cout << "[map_reads] " << name << " " << h_dist << endl;
+      if (STATS && VERBOSE) cout << "[map_reads] " << name << " " << e_dist << endl;
+      dist += h_dist;
+      edit += e_dist;
     }
     counter++;
-
-
+    if (locations.size() + reversed_locations.size() > max_size) {
+      max_size = locations.size() + reversed_locations.size();
+      cout << locations.size() + reversed_locations.size() << endl;
+    }
     locations.clear();
+    reversed_locations.clear();
   }
 
   average /= (counter / 4);
   dist /= (counter / 4);
+  edit /= (counter / 4);
   cout << "Average hamming dist: " << dist << endl;
+  cout << "Average edit dist: " << edit << endl;
   cout << "Average # of mapped locations: " << average << endl;
   cout << "Unmapped reads: " << unmapped << endl;
 }
@@ -267,8 +302,8 @@ int main(int argc, char *argv[]) {
   cout << "Preallocated vector with capacity: " << m.capacity() << endl;
   read_reference(argv[2], m);
   cout << "Reference genome has length: " << complete_ref.size() << endl;
-
   process_reference(m);
+  cout << "Finished processing reference" << endl;
   map_stats(m);
 
   map_reads(argv[1], m);
