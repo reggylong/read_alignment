23182660bb5b9c5be9f1ffcbbd0219527ab01ce7
Reginald Long <reggylong@gmail.com>
2016-08-29 19:31:49 -0700

    simplified reading reference setup

--- src/main.cc
+++ src/main.cc
@@ -17,15 +17,37 @@
 #include <algorithm>
 #include "semaphore.h"
 
-#define DEBUG false 
+#define DEBUG false
+#define VERBOSE false
 
 using namespace std;
 
-static const size_t LENGTH = 13;
+static const size_t LENGTH = 14;
 static const size_t LOCATIONS = 5;
 static const size_t SPACE = pow(4, LENGTH);
 static const long THREADS = 6;
 string complete_ref;
+
+char reverse_bp(char c) {
+  switch (toupper(c)) {
+    case 'A':
+      return 'T';
+    case 'C':
+      return 'G';
+    case 'G':
+      return 'C';
+    case 'T':
+      return 'A';
+    default:
+      return 'N';
+  }
+}
+
+void reverse_read(const string &read, string &reversed) {
+  for (int_fast32_t i = read.length() - 1; i >= 0; i--) {
+    reversed += reverse_bp(read[i]);
+  }
+}
 
 static uint_fast32_t chtoi(char c) {
   // A & 3 = 01
@@ -39,7 +61,6 @@ static uint_fast32_t chtoi(char c) {
 static bool ktoi(const string &read, const long pos, uint_fast32_t &index) {
   uint_fast32_t base = 1;
   uint_fast32_t total = 0;
-  if (DEBUG) assert(pos + LENGTH - 1 < read.length());
   for (int i = pos + LENGTH - 1; i >= pos; i--) {
     if (toupper(read[i]) == 'N') return false;
     total += base * chtoi(toupper(read[i]));
@@ -49,7 +70,10 @@ static bool ktoi(const string &read, const long pos, uint_fast32_t &index) {
   return true;
 }
 
-static bool add(vector<vector<uint_fast32_t>> &m, const string &read, long pos) {
+static bool add(vector<vector<uint_fast32_t>> &m, const string &read, size_t pos) {
+  if (pos == 33491329) {
+    cout << "HELLO WORLD" << endl;
+  }
   uint_fast32_t index = 0;
   if (ktoi(read, pos, index)) {
     m[index].emplace_back(pos);
@@ -59,31 +83,15 @@ static bool add(vector<vector<uint_fast32_t>> &m, const string &read, long pos) 
 }
 
 // Importantly, we do not make distinctions between chromosomes
-void process_chromosome(string &ref, vector<vector<uint_fast32_t>> &m, long pos) {
+void process_reference(vector<vector<uint_fast32_t>> &m) {
   string kmer = "";
 
-  for (size_t i = pos; i < ref.length() - LENGTH; i += LENGTH) {
-    add(m, ref, pos);
-    pos += LENGTH;
+  for (size_t i = 0; i < complete_ref.length() - LENGTH; i += LENGTH) {
+    add(m, complete_ref, i);
   }
 }
 
-long read_chromosome(ifstream &f, string &ref) {
-  string line;
-  while (getline(f, line)) {
-    if (line[0] == '>') {
-      // previous chromosome's length
-      cout << "Read " << ref.length() << " characters" << endl;
-      cout << line << endl;
-      break;
-    }
-    ref.append(line);
-  }
-  if (f.eof()) {
-    cout << "Read " << ref.length() << " characters" << endl;
-  }
-  return ref.length();
-}
+
 
 uint_fast32_t hamming(const string &read, uint_fast32_t pos) {
   uint_fast32_t dist = 0;
@@ -93,11 +101,35 @@ uint_fast32_t hamming(const string &read, uint_fast32_t pos) {
   return dist;
 }
 
+void compute_locations(const string &read, vector<vector<uint_fast32_t>> &m, vector<uint_fast32_t> &locations) {
+  size_t i = 0;
+  uint_fast32_t index = 0;
+  while (i < read.size() && (i < LENGTH || locations.size() < LOCATIONS)) {
+    if (ktoi(read, i, index)) {
+      for (uint_fast32_t j = 0; j < m[index].size(); j++) {
+        locations.push_back(m[index][j] - i);
+      }
+    }
+    i++;
+  }
+}
+
+uint_fast32_t argmin(vector<uint_fast32_t> &v) {
+  uint_fast32_t i = 0;
+  uint_fast32_t minimum = UINT_FAST32_MAX;
+  for (uint_fast32_t j = 0; j < v.size(); j++) {
+    if (v[j] < minimum) {
+      i = j;
+      minimum = v[j];
+    }
+  }
+  return i;
+}
 
 void map_reads(char *fastqname, vector<vector<uint_fast32_t>> &m) {
 
-  static vector<uint_fast32_t> locations;
-  locations.reserve(10000);
+  vector<uint_fast32_t> locations;
+  vector<uint_fast32_t> reversed_locations;
 
   ifstream f;
   f.open(fastqname);
@@ -114,24 +146,39 @@ void map_reads(char *fastqname, vector<vector<uint_fast32_t>> &m) {
       counter++;
       continue;
     }
-    
-    size_t i = 0;
-    uint_fast32_t index = 0;
-    while (i < line.size() && (i < LENGTH || locations.size() < LOCATIONS)) {
-      if (ktoi(line, i, index)) {
-        for (uint_fast32_t j = 0; j < m[index].size(); j++) {
-          locations.emplace_back(m[index][j] - i);
-        }
-      }
-      i++;
+
+    if (VERBOSE) {
+      cout << "[map_reads] Examining line: " << line << endl;
     }
+    compute_locations(line, m, locations);
+    string reversed_read;
+    reverse_read(line, reversed_read);
+
+    compute_locations(reversed_read, m, reversed_locations);
+    locations.insert(locations.end(), reversed_locations.begin(), reversed_locations.end());
+
     sort(locations.begin(), locations.end());
     locations.erase(unique(locations.begin(), locations.end()), locations.end());
     average += locations.size();
     if (locations.size() == 0) {
       unmapped++;
+    } else {
+      vector<uint_fast32_t> dists;
+      for (uint_fast32_t k = 0; k < locations.size(); k++) {
+        dists.emplace_back(hamming(line, locations[k]));
+      }
+      uint_fast32_t index = argmin(dists);
+      if (DEBUG) {
+        for (uint_fast32_t i = 0; i < locations.size(); i++) {
+          cout << complete_ref.substr(locations[i], 100) << " : " << locations[i] << endl;
+        }
+      }
+      dist += dists[index];
+      cout << "Index of min: " << locations[index] << endl;
     }
     counter++;
+
+
     locations.clear();
   }
 
@@ -149,22 +196,22 @@ void read_reference(char *fname, vector<vector<uint_fast32_t>> &m) {
 
   string line;
   // 0 index
-  long pos = 0;
   getline(f, line); // remove first header
   cout << line << endl;
 
-  string ref;
-  ref.reserve(500000000);
-  while (!f.eof()) {
-    long offset = read_chromosome(f, ref);
-    process_chromosome(ref, m, pos);
-    complete_ref.append(ref);
-    pos += offset;
-    ref.clear();
+  while (getline(f, line)) {
+    if (line[0] == '>') {
+      cout << line << endl;
+    } else {
+      complete_ref.append(line);
+    }
   }
+
   f.close();
 
-  /*
+}
+
+void map_stats(vector<vector<uint_fast32_t>> &m) {
   cout << "Size of map: " << m.size() << endl;
   double average = 0.0;
   long count = 0;
@@ -173,12 +220,8 @@ void read_reference(char *fname, vector<vector<uint_fast32_t>> &m) {
     if (!iter->empty()) count++;
   }
   average /= count;
-  cout << "Average # of elements: " << average << endl;
-  struct rusage r_usage;
-  getrusage(RUSAGE_SELF, &r_usage);
-  cout << "Memory usage " << r_usage.ru_maxrss * 1024 << endl;*/
+  cout << "Average # of elements per kmer: " << average << endl;
 }
-
 
 int main(int argc, char *argv[]) {
   ios::sync_with_stdio(false);
@@ -192,10 +235,15 @@ int main(int argc, char *argv[]) {
   vector<vector<uint_fast32_t>> m;
   m.resize(SPACE, vector<uint_fast32_t>(0));
   
-  //cout << "Preallocated vector with capacity: " << m.capacity() << endl;
+  cout << "Preallocated vector with capacity: " << m.capacity() << endl;
   read_reference(argv[2], m);
-  //cout << "Reference genome has length: " << complete_ref.size() << endl;
-  //map_reads(argv[1], m);
+  cout << "Reference genome has length: " << complete_ref.size() << endl;
 
+  process_reference(m);
+  map_stats(m);
+
+  map_reads(argv[1], m);
+
+  cout << complete_ref.substr(33491329, 100) << endl;
   return 0;
 }
